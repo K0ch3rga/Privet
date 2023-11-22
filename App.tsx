c730cca35cffacb4b01c458fbc6d4d37e48284f7
reffy33 <reffymin@gmail.com>
2023-11-22 20:03:25 +0500

    welcome screen added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './components/HelloScreen';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <WelcomeScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/HelloScreen.tsx
@@ -0,0 +1,15 @@
+import LogoWithText from './LogoWithText';
+import DropDown from './DropDown';
+
+
+const WelcomeScreen: React.FC = () => {
+  return (
+    <>
+      <LogoWithText/>
+      <DropDown />
+    </>
+  );
+};
+
+
+export default WelcomeScreen;
